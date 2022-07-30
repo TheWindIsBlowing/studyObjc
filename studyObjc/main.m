40ab79064c60e163a2a638dcdb8b4fec6f9f7327
yoka_build <yoka_build@unreachable.com>
2022-07-30 13:03:44 +0800

    macOs commandTool proj

---
+++ studyObjc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  studyObjc
+//
+//  Created by Yoka_build on 2022/7/30.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
